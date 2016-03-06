378bce5364ebeb160dc5e90da23344205d05ca47
Han Lee <hanlee@unist.ac.kr>
2016-03-07 00:28:21 +0900

    Apply Material Design Tab and Drawer

--- app/src/main/java/com/unist/hexa/chickenq/BoardActivity.java
+++ app/src/main/java/com/unist/hexa/chickenq/BoardActivity.java
@@ -1,75 +1,102 @@
 package com.unist.hexa.chickenq;
 
-import android.app.DialogFragment;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
-import android.widget.EditText;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-import com.unist.hexa.chickenq.util.BoardListAdapter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by user on 2015-08-06.
  */
-public class BoardActivity extends AppCompatActivity implements View.OnClickListener {
-    EditText et_search;
+public class BoardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        et_search = (EditText) findViewById(R.id.SearchEditText);
-        et_search.addTextChangedListener(textWatcher);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.tabanim_viewpager);
+        setupViewPager(viewPager);
 
-        findViewById(R.id.SearchButton).setOnClickListener(this);
-        findViewById(R.id.DetailButton).setOnClickListener(this);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabanim_tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
-        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.content_frame, new BoardFragment()).commit();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    TextWatcher textWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            BoardListAdapter.search_text = s.toString();
-            BoardFragment.mAdapter.dataChange();
-        }
-    };
-
+    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.SearchButton:
-                break;
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        switch (id){
+            default:
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
-            case R.id.DetailButton:
-                // DialogFragment.show() will take care of adding the fragment
-                // in a transaction.  We also want to remove any currently showing
-                // dialog, so make our own transaction and take care of that here.
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                Fragment prev = getFragmentManager().findFragmentByTag("dialog");
-                if (prev != null) {
-                    ft.remove(prev);
-                }
-                ft.addToBackStack(null);
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new BoardFragment(), "전체");
+        adapter.addFragment(new BoardFragment(), "치킨");
+        adapter.addFragment(new BoardFragment(), "피자");
+        adapter.addFragment(new BoardFragment(), "짜장면");
+        adapter.addFragment(new BoardFragment(), "탕수육");
+        adapter.addFragment(new BoardFragment(), "패스트푸드");
+        viewPager.setAdapter(adapter);
+    }
 
-                // Create and show the dialog.
-                DialogFragment newFragment = new BoardSearchDialogFragment();
-                newFragment.show(ft, "dialog");
-                break;
+    //ViewPager 을 위한 class
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        //Fragment를 List로 받는다.
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
         }
     }
 }
